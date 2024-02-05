ad11be298c2822e3a03a97a6977694c831c70210
Javier Marín <49601989+Javier1221@users.noreply.github.com>
2024-02-05 10:18:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
 function encript(){
-    let oracion=  document.getElementById('ingreso').value;
-    let minusculas= oracion.toLowerCase();
-    //alert(minusculas);
-    let cifrado= minusculas
-        .replace('e',"enter")
-        .replace('i',"imes")
-        .replace('a',"ai")
-        .replace('o',"ober")
-        .replace('u',"ufat");
-     document.querySelector('#mostrar').value =cifrado;
-    //alert(cifrado)
+    if(document.getElementById('ingreso').value==""){
+        alert("Por favor ingresa el texto a encriptar");
+        const copiado = document.getElementById('copy');
+        copiado.disabled = true; 
+    }else{
+        let oracion=  document.getElementById('ingreso').value;
+        let minusculas= oracion.toLowerCase();
+        //alert(minusculas);
+        let cifrado= minusculas
+            .replace('e',"enter")
+            .replace('i',"imes")
+            .replace('a',"ai")
+            .replace('o',"ober")
+            .replace('u',"ufat");
+         document.querySelector('#mostrar').value =cifrado;
+         const copiado = document.getElementById('copy');
+         copiado.disabled = false; 
+        //alert(cifrado)
+
+    }
+
 };
 
 function dencript(){
-    let oracion=  document.getElementById('ingreso').value;
-    let minusculas= oracion.toLowerCase();
-    //alert(minusculas);
-    let cifrado= minusculas
-        .replace("enter",'e')
-        .replace("imes",'i')
-        .replace("ai",'a')
-        .replace("ober",'o')
-        .replace("ufat",'u');
-        document.querySelector('#mostrar').value =cifrado;
-    //alert(cifrado)
+    if(document.getElementById('ingreso').value==""){
+        alert("Por favor ingresa el texto a desencriptar");
+    }else{
+        let oracion=  document.getElementById('ingreso').value;
+        let minusculas= oracion.toLowerCase();
+        //alert(minusculas);
+        let cifrado= minusculas
+            .replace("enter",'e')
+            .replace("imes",'i')
+            .replace("ai",'a')
+            .replace("ober",'o')
+            .replace("ufat",'u');
+            document.querySelector('#mostrar').value =cifrado;
+            const copiado = document.getElementById('copy');
+            copiado.disabled = false; 
+        //alert(cifrado)
+
+    }
 };
+
+function copiar(){
+    let copiaTexto = document.getElementById('mostrar').value;
+    copiaTexto.focus();
+    document.execCommand('selectAll');
+    document.execCommand('copy');
+
+}
